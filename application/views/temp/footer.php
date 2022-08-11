4d9a6976856661a86a7c8c49a350a676680c2dd2
alfandi2020 <pandibl4530@gmail.com>
2022-08-11 11:29:17 +0700

    aa

--- application/views/temp/footer.php
+++ application/views/temp/footer.php
@@ -59,8 +59,8 @@
       </div>
     </div> -->
     <div class="container">
-      <div class="copyright"> &copy; Copyright. All Rights Reserved</div>
-      <div class="credits"> Designed by BootstrapMade</div>
+      <div class="copyright"> &copy; Copyright. All Rights Reserved Menindo</div>
+      <!-- <div class="credits"> Designed by BootstrapMade</div> -->
     </div>
   </footer> <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
       class="bi bi-arrow-up-short"></i></a>
