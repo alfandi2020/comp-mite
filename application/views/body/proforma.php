188f499097d2202a1efdc82bac7674e000e35a16
alfandi2020 <pandibl4530@gmail.com>
2022-12-26 14:06:03 +0700

    a

--- application/views/body/proforma.php
+++ application/views/body/proforma.php
@@ -64,7 +64,7 @@
                                     Detail 
                                  </button>
                               <?php } ?>
-                              <a class="btn btn-primary btn-sm" href="https://login.menindo.com">Book now! <?= $_COOKIE['user'] ?></a>
+                              <a class="btn btn-primary btn-sm" href="https://login.menindo.com">Book now!</a>
                            </td>
                         </tr>
                         <tr>
